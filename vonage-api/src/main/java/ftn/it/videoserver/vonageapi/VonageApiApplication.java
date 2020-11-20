4251a78c20968c26871d6eefe512f01ba786082d
Ana <ana.rudic95@gmail.com>
2020-11-20 01:25:28 +0100

    First commit

---
+++ vonage-api/src/main/java/ftn/it/videoserver/vonageapi/VonageApiApplication.java
@@ -0,0 +1,16 @@
+package ftn.it.videoserver.vonageapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VonageApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VonageApiApplication.class, args);
+		System.out.println("Application started!");
+		
+		
+	}
+
+}
